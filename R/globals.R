e761a211355bbfc5d3b93843c10861b155a5a329
Martin Chan <martinchan53@gmail.com>
2022-10-24 15:22:29 +0800

    fix: R CMD checks

--- R/globals.R
+++ R/globals.R
@@ -50,6 +50,16 @@ utils::globalVariables(
     "value_rescaled",
     "Meeting_hours_with_manager_1_1",
     "Meetings_with_manager_1_on_1",
-    "Cadence_of_1_on_1_meetings_with_manager"
+    "Cadence_of_1_on_1_meetings_with_manager",
+    "Organization",
+    "flag_nkw",
+    "holidayweek",
+    "mean_collab",
+    "perc",
+    "perc_nkw",
+    "total",
+    "ymax",
+    "ymin",
+    "z_score"
     )
 )
