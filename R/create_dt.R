57d0e2c51ffcc69611a063a614c1c96a8046b83e
Martin Chan <martinchan53@gmail.com>
2023-08-23 11:36:11 +0100

    refactor: remove DT dependency

--- R/create_dt.R
+++ R/create_dt.R
@@ -17,7 +17,6 @@
 #' @param percent Logical value specifying whether to display numeric columns
 #' as percentages.
 #'
-#' @import DT
 #' @importFrom dplyr mutate_if
 #'
 #' @family Import and Export
@@ -28,45 +27,11 @@
 #' @export
 create_dt <- function(x, rounding = 1, freeze = 2, percent = FALSE){
 
-  # Round all numeric to "rounding" number of dp
-  num_cols <- dplyr::select_if(x, is.numeric) %>% names()
+  wpa::create_dt(
+    x = x,
+    rounding = rounding,
+    freeze = freeze,
+    percent = percent
+  )
 
-  if(length(num_cols) == 0){ # No numeric columns
-
-    out <-
-      DT::datatable(x,
-                  extensions = c('Buttons',
-                                 'FixedColumns'),
-                  options = list(dom = 'Blfrtip',
-                                 fixedColumns = list(leftColumns = freeze),
-                                 scrollX = TRUE,
-                                 buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
-                                 lengthMenu = list(c(10,25,50,-1),
-                                                   c(10,25,50,"All"))))
-
-  } else {
-
-    out <-
-      DT::datatable(x,
-                  extensions = c('Buttons',
-                                 'FixedColumns'),
-                  options = list(dom = 'Blfrtip',
-                                 fixedColumns = list(leftColumns = freeze),
-                                 scrollX = TRUE,
-                                 buttons = c('copy', 'csv', 'excel', 'pdf', 'print'),
-                                 lengthMenu = list(c(10,25,50,-1),
-                                                   c(10,25,50,"All")))) %>%
-      DT::formatRound(columns = num_cols, rounding)
-
-    if(percent == TRUE){
-
-      out <-
-        out %>%
-        DT::formatPercentage(columns = num_cols, rounding)
-
-    }
-  }
-
-
-  out
 }
