06458ac1196059d438817895f69e7686804db197
Martin Chan <martinchan53@gmail.com>
2025-05-28 09:48:47 +0100

    chore: remove vivainsights.Rcheck

--- vivainsights.Rcheck/00_pkg_src/vivainsights/inst/rmd_template/wellbeing/auxiliary.R
+++
@@ -1,8 +0,0 @@
-md2html <- function(text){
-
-  html_chunk <- markdown::markdownToHTML(text = text,
-                                         fragment.only = TRUE)
-
-  htmltools::HTML(html_chunk)
-
-}
