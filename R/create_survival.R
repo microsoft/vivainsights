86236a68426c64b0092e8129a58c0aa6c4f7727d
Sachin Gaikwad (HCL Technologies Corporate Services) <v-gaikwadsac@microsoft.com>
2025-10-17 21:12:59 +0530

    Revert "Update R/create_survival.R"
    This reverts commit e425babac41779365f04bf33a0bd5bc0aed1f4a7.

--- R/create_survival.R
+++ R/create_survival.R
@@ -82,8 +82,8 @@ DEFAULT_COPILOT_METRICS <- c(
   "Copilot_actions_taken_in_Powerpoint"
 )
 
-# Infix helpers
-source("R/utils-infix.R")
+`%||%` <- function(a,b) if (!is.null(a)) a else b
+
 # ---------- Helpers ----------
 ##' Canonicalize segment labels
 ##'
